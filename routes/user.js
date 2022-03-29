e9ae4c4f024bb9cb06deeda0f4a71c3918bc0482
Vitor Cecilio <vtr.cecilio@gmail.com>
2022-03-29 09:50:34 -0300

    esquema

--- routes/user.js
+++ routes/user.js
@@ -4,24 +4,41 @@ const User = require('../models/user');
 
 const jwt = require('jsonwebtoken');
 
-router.post('/create', async (req, res) => {
+router.post('/create-loja', async (req, res) => {
+    const user = new User({...req.body, tipoUser: "Loja"});
+
+    try{
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.status(201).json({
+            success: "Usuário Loja criado com sucesso!",
+            token: token
+        });
+    } catch(e){
+        console.log(e);
+        console.log(e.message);
+        res.status(400).json({error: 'Não foi possível criar o usuário Loja!'});
+        return;
+    }
+});
+
+router.post('/create-cliente', async (req, res) => {
     const user = new User({...req.body, tipoUser: "Cliente"});
 
     try{
         await user.save();
         const token = await user.generateAuthToken();
         res.status(201).json({
-            success: "Usuário criado com sucesso!",
+            success: "Usuário Cliente criado com sucesso!",
             token: token
         });
     } catch(e){
         console.log(e);
         console.log(e.message);
-        res.status(400).json({error: 'Não foi possível criar o usuário!'});
+        res.status(400).json({error: 'Não foi possível criar o usuário Cliente!'});
         return;
     }
 });
-
 
 router.post('/login', async (req, res) => {
     try {
