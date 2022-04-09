8a152ef6e4c5932900eed0788e03a62ccb19d054
Vitor Cecilio <vtr.cecilio@gmail.com>
2022-04-09 18:47:56 -0300

    Autenticacao em fase de validacao

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, 'thisismycourse')
+    
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token});
+    
+        if (!user){
+            throw new Error();
+        }
+
+        req.user = user
+
+        next();
+    } catch (e) {
+        res.status(401).send({ error: "Não foi possível autenticar!"});
+    }
+};
+
+
+module.exports = router;
